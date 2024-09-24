8c19af61c880f261db2399fefbfe087371d8ae3c
gdprasad1201 <gdprasad@crimson.ua.edu>
2024-09-24 12:57:23 -0500

    commit

--- HW 2/hw1.c
+++ HW 2/hw1.c
@@ -13,7 +13,7 @@ double gettime() {
 }
 
 // Function to print the board
-void printBoard(int **board, int N) {
+void printBoard(int** board, int N) {
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
             if (board[i][j]) {
@@ -26,7 +26,7 @@ void printBoard(int **board, int N) {
     }
 }
 
-void generateBoard(int **board, int N) {
+void generateBoard(int** board, int N) {
     for (int i = 0; i < N + 2; i++) {
         board[0][i] = board[i][0] = board[N + 1][i] = board[i][N + 1] = 0;
     }
@@ -40,15 +40,13 @@ void generateBoard(int **board, int N) {
 }
 
 // Function to count the number of live neighbors
-int countLiveNeighbors(int **board, int i, int j) {
-    return board[i - 1][j - 1] + board[i - 1][j] + board[i - 1][j + 1] +
-           board[i][j - 1] + board[i][j + 1] + board[i + 1][j - 1] +
-           board[i + 1][j] + board[i + 1][j + 1];
+int countLiveNeighbors(int** board, int i, int j) {
+    return board[i - 1][j - 1] + board[i - 1][j] + board[i - 1][j + 1] + board[i][j - 1] + board[i][j + 1] + board[i + 1][j - 1] + board[i + 1][j] + board[i + 1][j + 1];
 }
 
 // Function to simulate the game of board
-void gameOfLife(int **board, int N, int maxGenerations) {
-    int **newBoard = board;  // Create a new board to store the next generation
+void gameOfLife(int** board, int N, int maxGenerations) {
+    int** newBoard = board;  // Create a new board to store the next generation
     int generation = 0;      // Generation counter
 
     while (generation < maxGenerations) {  // Run the simulation for the specified number of generations
@@ -95,9 +93,9 @@ int main(int argc, char **argv) {
     int N = atoi(argv[1]), maxGenerations = atoi(argv[2]);     // Size of the board and maximum number of generations
     double timeTaken[3];  // Array to store the time taken for each run
 
-    int **board = (int **)malloc((N + 2) * sizeof(int *));
+    int** board = malloc((N + 2) * sizeof(int*));
     for (int i = 0; i < N + 2; i++) {
-        board[i] = (int *)malloc((N + 2) * sizeof(int));
+        board[i] = malloc((N + 2) * sizeof(int));
     }
 
     for (int i = 0; i < 3; i++) { // Run the simulation 3 times
@@ -111,14 +109,6 @@ int main(int argc, char **argv) {
     }
 
     printf("Time taken: %f seconds\n", (timeTaken[0] + timeTaken[1] + timeTaken[2]) / 3); // Print the average time taken for the 3 runs
-
-    // generateBoard(board, N);
-
-    // double start = gettime();              // Start the timer
-    // gameOfLife(board, N, maxGenerations);  // Run the game of board simulation
-    // double end = gettime();                // End the timer
-
-    // printf("Time taken: %f seconds\n", end - start);
 
     for (int i = 0; i < N + 2; i++) {
         free(board[i]);
