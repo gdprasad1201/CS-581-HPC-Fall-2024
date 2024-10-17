e37822ba514eec5452afa9525ed1d9119a756791
gdprasad1201 <gdprasad@crimson.ua.edu>
2024-10-17 16:04:04 -0500

    commit

--- HW 3/hw3.c
+++ HW 3/hw3.c
@@ -4,6 +4,7 @@
    Course Section: CS 581
    Homework #: 3
    Instructions to compile the program: gcc -Wall -o hw3 hw3.c -fopenmp
+   Instructions to compile the program with debugging: gcc -Wall -o hw3 hw3.c -fopenmp -DDEBUG
    Instructions to run the program: ./hw3 <size of board> <max iterations> <number of threads> <output file directory>
 */
 
@@ -47,7 +48,7 @@ void generateBoard(int** board, int N) {
     }
 }
 
-int **allocarray(int P, int Q) {
+int** allocarray(int P, int Q) {
     int i, *p, **a;
 
     p = (int *)malloc(P * Q * sizeof(int));
@@ -58,9 +59,16 @@ int **allocarray(int P, int Q) {
 }
 
 // Free the memory allocated to the board
-void freearray(int **a) {
+void freearray(int** a) {
     free(&a[0][0]);
     free(a);
+}
+
+void swap(int*** a, int*** b) {
+    int** temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int main(int argc, char** argv) {
@@ -69,74 +77,107 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    int N = atoi(argv[1]), maxGenerations = atoi(argv[2]), numThreads = atoi(argv[3]), i, j, tid, generation;
+    int N = atoi(argv[1]), maxGenerations = atoi(argv[2]), numThreads = atoi(argv[3]), i, j, generation, recordGen = 0;
     bool change;
 
     int** board = allocarray(N + 2, N + 2);
     int** newBoard = allocarray(N + 2, N + 2);
-    int** temp;
 
     generateBoard(board, N);
     generateBoard(newBoard, N);
 
     double start = omp_get_wtime();
 
-    // Compute the next generation of the board
-    for (generation = 1; generation <= maxGenerations; generation++) {
-       change = false;
+    // Update the board in parallel using OpenMP threads
+    #pragma omp parallel default(none) shared(recordGen, change, numThreads, board, newBoard, maxGenerations, N) private(i, j) num_threads(numThreads) reduction(+:generation)
+    {
+        int tid = omp_get_thread_num();
+        int nthreads = omp_get_num_threads();
+        int iStart = (N / nthreads) * tid + 1;
+        int iEnd = (N / nthreads) * (tid + 1);
 
-        // Update the board in parallel using OpenMP threads
-        #pragma omp parallel num_threads(numThreads) private(i, j, tid) shared(generation, board, newBoard, change, maxGenerations, N)
-        {
-            tid = omp_get_thread_num(); // Get the thread ID
-            int start = tid * (N / numThreads) + 1; // Calculate the start and end indices for each thread
-            int end = (tid + 1) * (N / numThreads); // Calculate the start and end indices for each thread
+        if (tid == nthreads - 1) {
+            iEnd = N;
+        }
 
-            // Update the start and end indices for the last thread
-            if (tid == numThreads - 1) {
-                end += (N % numThreads);
-            }
+        #ifdef DEBUG
+            printf("tid=%d iStart=%d iEnd=%d\n", tid, iStart, iEnd);
+        #endif
 
-            for (i = start; i <= end; i++) {
+        for (generation = 0; generation < maxGenerations; generation+=1) {
+            bool localChange = false;
+            change = false;
+
+            for (i = iStart; i <= iEnd; i++) {
                 for (j = 1; j <= N; j++) {
                     int liveNeighbors = board[i - 1][j - 1] + board[i - 1][j] + board[i - 1][j + 1] + board[i][j - 1] + board[i][j + 1] + board[i + 1][j - 1] + board[i + 1][j] + board[i + 1][j + 1];
                     if (board[i][j]) {
                         if (liveNeighbors < 2 || liveNeighbors > 3) {
                             newBoard[i][j] = DEAD;
-                            change = true;
+                            localChange = true;
                         } else {
                             newBoard[i][j] = ALIVE;
                         }
                     } else if (!board[i][j]) {
                         if (liveNeighbors == 3) {
                             newBoard[i][j] = ALIVE;
-                            change = true;
+                            localChange = true;
                         } else {
                             newBoard[i][j] = DEAD;
                         }
                     }
                 }
             }
-        }
-
-        #pragma omp barrier 
-        {
-            if (!change) {
-                break;
+            
+            #pragma omp critical
+            {
+                // if (localChange) {
+                //     change = true;
+                // }
+               change = change || localChange;
             }
-            temp = board;
-            board = newBoard;
-            newBoard = temp;
+
+            #pragma omp barrier 
+            
+            #pragma omp single
+            {
+                if (!change) {
+                    recordGen = generation;
+                    generation = maxGenerations;
+                }
+            }
+            
+            #pragma omp single
+            {  
+                swap(&board, &newBoard);
+                #ifdef DEBUG
+                    if(generation != maxGenerations){
+                        printf("Generation %d:\n", generation);
+                        printBoard(board, N); 
+                    }
+                    
+                #endif
+            }    
         }
     }
+    
 
     double end = omp_get_wtime();
 
+   // freearray(temp);
     freearray(newBoard);
+    #ifdef DEBUG
+        printf("Final board:\n");
+        printBoard(board, N);  // Print the final board
 
-    // printf("Final board:\n");
-    // printBoard(board, N);  // Print the final board
-
+        if (recordGen) {
+            printf("The Game of Life ends at iteration %d\n", recordGen);
+        }
+        else {
+            printf("The Game of Life has reached the maximum number of iterations of %d.\n", atoi(argv[2]));
+        }
+    #endif
+        
     printf("%d x %d board computed with %d maximum iterations and %d threads:\n", N, N, maxGenerations, numThreads);
     printf("\t\tTime taken: %lf seconds\n\n", end - start);  // Print the time taken
     
